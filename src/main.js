28a68684135e21314e6c91439fc398d3ffa6a7ee
Jiří Kofroň <jiri.kofron@signi.com>
2023-02-01 00:03:22 +0100

    project init, running Phaser component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader';
+
+defineIonPhaser(window)
+
 createApp(App).mount('#app')
